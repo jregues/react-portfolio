9eb5f7e0005a23ba71faf3b37f11e083f829a250
jregues <jregues@me.com>
2024-05-21 13:08:28 -0400

    Added code to Home and About pages

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Me</h1>
+            <p>I am a 24 year old aspiring web developer currently attending the UNCC Coding Bootcamp. I have been practicing Python, HTML, and CSS for the last year, and have learned JavaScript, APIs, NodeJS, Express, MySQL, NoSQL Handlebars, Postgres, MongoDb, MERN, and React during my 3 months in the bootcamp.</p>
+        </div>
+    );
+}
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome!</h1>
+            <p>My name is James Egues and this is a small portfolio created by me for my module 20 Challenge assignment for the UNCC Coding Bootcamp. Bellow are links to several github repositories for previous projects. Feel free to check them out by clicking on the images!</p>
+            <div className='repos'>
+                <img src='' href=''></img>
+                <img src='' href=''></img>
+                <img src='' href=''></img>
+                <img src='' href=''></img>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Home;
